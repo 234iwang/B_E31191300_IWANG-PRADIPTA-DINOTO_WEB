21af11d9f19cdb90aaaed5b9de08f4e7527ec5f7
234iwang <66737765+234iwang@users.noreply.github.com>
2021-03-15 22:16:18 +0700

    MODUL 5

--- Latihan.php
+++ Latihan.php
@@ -1,35 +1,75 @@
 <?php
 
-class Laptop {
-    public $pemilik, $merk;
+interface hitungLuas {
+    public function hitungLuasPersegi($sisi);
 
-    public function __construct($pemilik = "Pemilik", $merk = "Merk")
+    public function hitungLuasSegitiga($sisi);
+
+    public function hitungLuasLingkaran($sisi);
+}
+
+class Persegi implements hitungLuas {
+
+    public function hitungLuasPersegi($sisi)
     {
-        $this->pemilik = $pemilik;
-        $this->merk = $merk;
+        return $sisi * $sisi;
     }
 
-    public function hidupkan_laptop()
+    public function hitungLuasSegitiga($sisi)
     {
-        return "Hidupkan laptop {$this->merk} punya {$this->pemilik}";
+        return "Error : Objek bukan segitiga.";
     }
 
-    public function matikan_laptop()
+    public function hitungLuasLingkaran($sisi)
     {
-        return "Matikan laptop {$this->merk} punya {$this->pemilik}";
-    }
-
-    public function restart_laptop()
-    {
-        return "Matikan laptop {$this->merk} punya {$this->pemilik}. Hidupkan laptop {$this->merk} punya {$this->pemilik}";
+        return "Error : Objek bukan Lingkaran";
     }
 }
 
-$laptop1 = new Laptop("A", "ASUS");
-echo $laptop1->hidupkan_laptop();
-echo "<br><br>";
-$laptop1 = new Laptop("B", "Acer");
-echo $laptop1->matikan_laptop();
-echo "<br><br>";
-$laptop1 = new Laptop("C", "Lenovo");
-echo $laptop1->restart_laptop();
+class Segitiga implements hitungLuas {
+
+    public function hitungLuasPersegi($sisi)
+    {
+        return "Error : Objek bukan persegi";
+    }
+
+    public function hitungLuasSegitiga($sisi)
+    {
+        return (1/2) * $sisi * $sisi;
+    }
+
+    public function hitungLuasLingkaran($sisi)
+    {
+        return "Error : Objek bukan lingkaran";
+    }
+}
+
+class Lingkaran implements hitungLuas {
+
+    public function hitungLuasPersegi($sisi)
+    {
+        return "Error : Objek bukan persegi";
+    }
+
+    public function hitungLuasSegitiga($sisi)
+    {
+        return "Error : Objek bukan segitiga";
+    }
+
+    public function hitungLuasLingkaran($sisi)
+    {
+        return $sisi * $sisi * pi();
+    }
+}
+
+$objek1 = new Persegi();
+$objek2 = new Segitiga();
+$objek3 = new Lingkaran();
+
+echo "<b>Jika luas sisi = 3</b><hr>";
+echo "Luas Persegi = {$objek1->hitungLuasPersegi(3)}";
+echo '<br>';
+echo "Luas Segitiga = 
+{$objek2->hitungLuasSegitiga(3)}";
+echo '<br>';
+echo "Luas Lingkaran = {$objek3->hitungLuasLingkaran(3)}";
