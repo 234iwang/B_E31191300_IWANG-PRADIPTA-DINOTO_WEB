67cff1429d50c5209f8ed8d96d9a0e5aff33fd64
234iwang <66737765+234iwang@users.noreply.github.com>
2021-04-12 22:49:35 +0700

    Add files via upload

---
+++ Minggu_9/application/models/Hello_model.php
@@ -0,0 +1,12 @@
+<?php
+class Hello_model extends CI_Model{
+    //public $txt = 'Hello World!';
+    
+    public $alert = 'Hello Wolrd dari CI Model';
+    
+    public $mahasiswa = [
+			["nama"=>"IWANG PRADIPTA DINOTO","nim"=>"E31191300","gol"=>"B"],
+			["nama"=>"ABIL FIAZ IBNU ROMADANI ","nim"=>"E31190801","gol"=>"B"],
+			["nama"=>"FIKI SANKRA WIJAYA","nim"=>"E31191334","gol"=>"B"] 
+		];
+}
